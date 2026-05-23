6c6a2bd0498fb51954d53308cdbb78e16f829997
Vergil <albmagerit@gmail.com>
2026-05-24 00:47:19 +0200

    Initial Fabric mod template

---
+++ src/client/java/com/y4mato/portalanchor/client/PortalAnchorClient.java
@@ -0,0 +1,10 @@
+package com.y4mato.portalanchor.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PortalAnchorClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
